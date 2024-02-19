b8c957e0b8bc2702b2f3f21b0644c7dba3d97c23
Your Name <you@example.com>
2024-02-19 12:04:26 +0000

    add log in screen

--- App.js
+++ App.js
@@ -5,81 +5,13 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import "./global.css";
-
-
-function HomeScreen() {
-
-    return (
-        <ScrollView>
-            <View
-                className="flex gap-y-2 flex-1 justify-center items-center text-3xl bg-[#161619]"
-            >
-                <View className="flex-row gap-x-2">
-                    <View className="w-16 h-16 bg-[#ef276f]" />
-                    <View className="w-16 h-16 bg-[#82df37]" />
-                    <View className="w-16 h-16 bg-[#d75151]" />
-                    <View className="w-16 h-16 bg-[#abe267]" />
-                    <View className="w-16 h-16 bg-[#25b1c7]" />
-                    <View className="w-16 h-16 bg-[#e8c940]" />
-                </View>
-
-                <Text className="text-3xl text-white">Text!</Text>
-
-                <View className="flex flex-row gap-x-4 items-center">
-                    <Text className="text-3xl text-[#ffd931]">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#ffd931] text-black">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#ffd931] text-[#ffd931]">Text!</Text>
-                </View>
-
-                <View className="flex flex-row gap-x-4 items-center">
-                    <Text className="text-3xl text-[#e8c940]">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#e8c940] text-black">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#e8c940] text-[#e8c940]">Text!</Text>
-                </View>
-
-                <View className="flex flex-row gap-x-4 items-center">
-                    <Text className="text-3xl text-[#ef276f]">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#ef276f] text-white">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#ef276f] text-[#ef276f]">Text!</Text>
-                </View>
-
-                <View className="flex flex-row gap-x-4 items-center">
-                    <Text className="text-3xl text-[#82df37]">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#82df37] text-white">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#82df37] text-[#82df37]">Text!</Text>
-                </View>
-
-                <View className="flex flex-row gap-x-4 items-center">
-                    <Text className="text-3xl text-[#d75151]">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#d75151] text-white">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#d75151] text-[#d75151]">Text!</Text>
-                </View>
-
-
-                <View className="flex flex-row gap-x-4 items-center">
-                    <Text className="text-3xl text-[#abe267]">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#abe267] text-white">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#abe267] text-[#abe267]">Text!</Text>
-                </View>
-
-
-                <View className="flex flex-row gap-x-4 items-center">
-                    <Text className="text-3xl text-[#25b1c7]">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#25b1c7] text-white">Text!</Text>
-                    <Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#25b1c7] text-[#25b1c7]">Text!</Text>
-                </View>
-
-                <View className="flex-row gap-x-4 items-center">
-                    <View className="w-64 h-16 bg-gradient-to-r from-[#ef276f] to-[#25b1c7]">
-                        <Text>doesnt work on mobile</Text>
-                    </View>
-                </View>
-            </View>
-        </ScrollView>
-    );
-}
+import HomeScreen from './src/screens/HomeScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+import { registerRootComponent } from 'expo';
 
 const Tab = createBottomTabNavigator();
+
+registerRootComponent(App)
 
 export default function App() {
     return (
@@ -89,6 +21,11 @@ export default function App() {
                     name="Home"
                     component={HomeScreen}
                     options={{ tabBarIcon: makeIconRender("home") }}
+                />
+                <Tab.Screen
+                    name="signup"
+                    component={SignUpScreen}
+                    options={{ tabBarIcon: makeIconRender("cog") }}
                 />
             </Tab.Navigator>
         </NavigationContainer>
---
+++ src/screens/SignUpScreen.jsx
@@ -0,0 +1,63 @@
+import { Text, View, StyleSheet, SafeAreaView, TextInput, Pressable } from "react-native";
+import { useState } from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+export default function SignUp() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [hidePass, setHidePass] = useState(true);
+
+    const toggleHidePass = () => {
+        setHidePass(!hidePass);
+    };
+
+    return (
+        <View className="flex-1 items-center justify-center">
+            <TextInput
+                className="border-2 p-2 m-2 rounded-xl"
+                onChangeText={(e) => {
+                    setUsername(e);
+                }}
+                value={username}
+                placeholder="username"
+                placeholderTextColor="gray"
+            />
+            <View className="flex-row">
+                <TextInput
+                    className="border-2 p-2 m-2 rounded-xl"
+                    value={password}
+                    onChangeText={(e) => {
+                        setPassword(e);
+                    }}
+                    placeholder="password"
+                    placeholderTextColor="gray"
+                    secureTextEntry={hidePass}
+                />
+                <View className="absolute right-5 self-center ">
+                    <MaterialCommunityIcons
+                        name={hidePass ? "eye-off" : "eye"}
+                        size={24}
+                        color="#aaa"
+                        onPress={toggleHidePass}
+                    />
+                </View>
+            </View>
+            <Pressable className="rounded-full bg-black text-white py-2 px-5 m-2">
+                {/* user info will be verified using supabase onPress */}
+                Log In
+            </Pressable>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 2,
+        padding: 10,
+    },
+    words: {
+        color: "blue",
+    },
+});
