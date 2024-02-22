37bbf71e3ace42dfb57907ce0888ba5a488b65bd
sw212 <sw212@github.com>
2024-02-22 12:23:24 +0000

    add LinearGradient to WorkoutCard

--- src/components/WorkoutCard.jsx
+++ src/components/WorkoutCard.jsx
@@ -2,6 +2,7 @@ import { useMemo } from "react";
 import { View, Text } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function WorkoutCard({ workout }) {
     const icon = useMemo(
@@ -16,7 +17,24 @@ export default function WorkoutCard({ workout }) {
 
     return (
         <View className="py-2">
-            <View className="min-w-[75%] mx-auto py-2 px-4 border border-white rounded-xl">
+            {/* <View className="min-w-[75%] mx-auto py-2 px-4 border border-white rounded-xl" /> */}
+            <LinearGradient
+                style={{
+                    minWidth: "75%",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    paddingVertical: 8,
+                    paddingHorizontal: 16,
+                    border: "solid",
+                    borderWidth: 1,
+                    borderColor: "white",
+                    borderRadius: 12,
+                }}
+                colors={["rgba(222, 67, 216, 0.1)", "transparent"]}
+                // colors={["#4c669f", "#3b5998", "#192f6a"]}
+                start={{ y: 0, x: 0 }}
+                end={{ y: 1, x: 1 }}
+            >
                 <View className="flex-row pb-2 justify-between">
                     <View className="flex-row pr-8 gap-x-4">
                         <Text className="text-white">{workout.type}</Text>
@@ -26,7 +44,7 @@ export default function WorkoutCard({ workout }) {
                 </View>
 
                 <View>{icon[workout.type]}</View>
-            </View>
+            </LinearGradient>
         </View>
     );
 }
