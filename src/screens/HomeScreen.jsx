848334a598f16d28d509af78ca082ff4bba46e09
sw212 <sw212@github.com>
2024-02-22 09:22:45 +0000

    update NeonGrid

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -93,8 +93,11 @@ export default function HomeScreen() {
         <>
             <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
                 <View className="h-full pt-4" ref={containerRef}>
+                    {/* <View className="w-[700px] h-[500px] mx-auto">
+                        <NeonGrid />
+                    </View> */}
                     <View className="w-[700px] h-[100px] mx-auto">
-                        <PointsBar frac={1.0} />
+                        <PointsBar frac={0.4} />
                     </View>
                     <View className="w-[700px] h-[200px] mx-auto">
                         <NeonRoundedRect />
@@ -110,8 +113,6 @@ export default function HomeScreen() {
                         <View className="relative w-32 h-16 bg-[#d75151] rounded-xl" ref={targetRef}>
                             <View className="absolute left-0 top-0 right-0 bottom-0"></View>
                         </View>
-                        <View className="w-16 h-16 bg-[#abe267]" />
-                        <View className="w-16 h-16 bg-[#25b1c7]" />
                     </View>
 
                     <View className="absolute left-0 top-0 right-0 bottom-0 -z-10">
@@ -133,71 +134,3 @@ export default function HomeScreen() {
         </>
     );
 }
-
-/*
-<View className="flex-row gap-x-2">
-<View className="w-16 h-16 bg-[#ef276f]" />
-<View className="w-16 h-16 bg-[#82df37]" />
-<View className="w-16 h-16 bg-[#d75151]" />
-<View className="w-16 h-16 bg-[#abe267]" />
-<View className="w-16 h-16 bg-[#25b1c7]" />
-<View className="w-16 h-16 bg-[#e8c940]" />
-</View>
-
-<Text className="text-3xl text-white">Text!</Text>
-
-<View className="flex flex-row gap-x-4 items-center">
-<Text className="text-3xl text-[#ffd931]">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#ffd931] text-black">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#ffd931] text-[#ffd931]">
-    Text!
-</Text>
-</View>
-
-<View className="flex flex-row gap-x-4 items-center">
-<Text className="text-3xl text-[#e8c940]">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#e8c940] text-black">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#e8c940] text-[#e8c940]">
-    Text!
-</Text>
-</View>
-
-<View className="flex flex-row gap-x-4 items-center">
-<Text className="text-3xl text-[#ef276f]">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#ef276f] text-white">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#ef276f] text-[#ef276f]">
-    Text!
-</Text>
-</View>
-
-<View className="flex flex-row gap-x-4 items-center">
-<Text className="text-3xl text-[#82df37]">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#82df37] text-white">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#82df37] text-[#82df37]">
-    Text!
-</Text>
-</View>
-
-<View className="flex flex-row gap-x-4 items-center">
-<Text className="text-3xl text-[#d75151]">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#d75151] text-white">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#d75151] text-[#d75151]">
-    Text!
-</Text>
-</View>
-
-<View className="flex flex-row gap-x-4 items-center">
-<Text className="text-3xl text-[#abe267]">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#abe267] text-white">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#abe267] text-[#abe267]">
-    Text!
-</Text>
-</View>
-
-<View className="flex flex-row gap-x-4 items-center">
-<Text className="text-3xl text-[#25b1c7]">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl bg-[#25b1c7] text-white">Text!</Text>
-<Text className="text-3xl p-4 w-44 text-center rounded-3xl border-solid border-2 border-[#25b1c7] text-[#25b1c7]">
-    Text!
-</Text>
-</View> */
