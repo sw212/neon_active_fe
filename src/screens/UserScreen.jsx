fd8d170970dfd5d2e55d397c74e11216ce921016
sw212 <94310203+sw212@users.noreply.github.com>
2024-02-27 14:10:26 +0000

    fix: user redirect on logout

--- src/screens/UserScreen.jsx
+++ src/screens/UserScreen.jsx
@@ -9,7 +9,7 @@ export default function UserScreen({ navigation }) {
         <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
             <View className="w-full bg-gray-200">
                 <View className="flex items-center w-[80%] max-w-md mx-auto py-3">
-                    <Text className="text-white text-2xl mb-2 self-center">Hello {user.firstName}</Text>
+                    <Text className="text-white text-2xl mb-2 self-center">Hello {user?.firstName}</Text>
                 </View>
                 <View className="flex items-center justify-between mx-auto px-4 py-2 max-w-md">
                     <Pressable
