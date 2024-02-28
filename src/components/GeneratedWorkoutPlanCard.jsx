7dc59dd3631ac6f359bda5af40177eadec08a374
Saroj <saroj_tamang@aol.com>
2024-02-28 14:59:25 +0000

    checkbox styling

--- src/components/GeneratedWorkoutPlanCard.jsx
+++ src/components/GeneratedWorkoutPlanCard.jsx
@@ -1,11 +1,30 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Dimensions, Text, View } from "react-native";
+import { CheckBox } from "react-native-btr";
 
-export default function GeneratedWorkoutPlanCard({item}) {
-console.log(item, "gwpcard")
+export default function GeneratedWorkoutPlanCard({ item }) {
+    console.log(item, "gwpcard");
+    const [isChecked, setisChecked] = useState(false);
 
-return (
-    <View className="flex-wrap justify-between m-5 h-full w-full border border-white rounded-xl">
-    <Text className="text-white text-2xl text-wrap p-3">{item}</Text>
-    </View>
-)    
+    return (
+        <View className="left-1 right-1 mx-6 self-center flex-1 flex-row flex-wrap justify-between my-5 w-80 border border-white rounded-xl">
+            <Text className="text-white text-lg text-wrap p-3 ">{item}</Text>
+            {/* <View className='m-5'> */}
+            <View className='m-4 rounded-xl'>
+                <Text className="bg-white p-1 rounded-xl">Workout Completed?</Text>
+            </View>
+            <View className='m-5'>
+                <CheckBox
+                    checked={isChecked}
+                    color="#fff"
+                    onPress={() => {
+                        setisChecked(!isChecked);
+                    }}
+                />
+            </View>
+            {/* </View> */}
+        </View>
+    );
 }
+
+// this styling works for ANDROID not Iphone
--- src/screens/DisplayWorkoutScreen.jsx
+++ src/screens/DisplayWorkoutScreen.jsx
@@ -2,29 +2,44 @@ import { View, Text } from "react-native";
 import { FlatList, ScrollView } from "react-native-gesture-handler";
 import GeneratedWorkoutPlanCard from "../components/GeneratedWorkoutPlanCard";
 import NeonBackground from "../components/shaders/NeonBackground";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function DisplayWorkoutScreen({ route }) {
     const { workoutPlan } = route.params[0];
     const daysArray = workoutPlan.split(/\n(?=Day \d+:)/).map((day) => day.trim());
     return (
         <>
-        <View className="flex-1 justify-center items-center">
-            <FlatList
-                data={daysArray}
-                renderItem={({ item }) => {
-                    console.log(item);
-                    if (item !== ""){
-                        return <GeneratedWorkoutPlanCard item={item} />;
-                    }
-                    
+            <LinearGradient
+                
+                style={{
+                    height: "100%",
                 }}
-                horizontal
-                showsHorizontalScrollIndicator
-                pagingEnabled
-                bounces={false}
-                />
-        </View>
-        <NeonBackground/>
-                </>
+                colors={["rgba(222, 67, 216, 0.1)", "transparent"]}
+                start={{ y: 0, x: 0 }}
+                end={{ y: 1, x: 1 }}
+
+            >
+                <Text className='top-7 left-20 text-white text-3xl'>Here is your workout</Text>
+                <View className="mx-5 my-4 flex-1 justify-center items-center">
+                    <FlatList
+                        snapToAlignment={"center"}
+                        className=""
+                        data={daysArray}
+                        renderItem={({ item }) => {
+                            if (item !== "") {
+                                return <GeneratedWorkoutPlanCard item={item} />;
+                            }
+                        }}
+                        horizontal
+                        showsHorizontalScrollIndicator
+                        pagingEnabled
+                        // bounces={false}
+                    />
+                </View>
+            </LinearGradient>
+            <NeonBackground />
+        </>
     );
 }
+
+//Alignment center works for android but not apple.
